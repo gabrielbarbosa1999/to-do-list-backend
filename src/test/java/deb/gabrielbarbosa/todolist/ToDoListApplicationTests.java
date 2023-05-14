b3233396af30840ac34710c22aea6be06d66c472
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2023-05-14 18:17:15 -0300

    Start Project

---
+++ src/test/java/deb/gabrielbarbosa/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package deb.gabrielbarbosa.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
