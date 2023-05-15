914587d87e7b922def386aaff1890623cd768d80
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2023-05-14 22:10:25 -0300

    Seed database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_to_do (to_do, created_at) VALUES ('Escreva três coisas pelas quais você é grato hoje em um diário de gratidão.', CURRENT_TIMESTAMP);
+INSERT INTO tb_to_do (to_do, created_at) VALUES ('Faça uma caminhada de 30 minutos ao ar livre e aproveite o ambiente ao seu redor.', CURRENT_TIMESTAMP);
+INSERT INTO tb_to_do (to_do, created_at) VALUES ('Reserve 15 minutos para ler um livro ou artigo de seu interesse.', CURRENT_TIMESTAMP);
+INSERT INTO tb_to_do (to_do, created_at) VALUES ('Aprenda uma nova palavra em um idioma diferente e tente usá-la ao longo do dia.', CURRENT_TIMESTAMP);
+INSERT INTO tb_to_do (to_do, created_at) VALUES ('Dedique 10 minutos para praticar exercícios de respiração profunda e relaxamento.', CURRENT_TIMESTAMP);
