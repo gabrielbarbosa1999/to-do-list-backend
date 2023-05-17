255d5fbcda6257f6d62d63b7a57a27657605cd1e
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2023-05-17 09:26:08 -0300

    Update CORS

--- src/main/java/deb/gabrielbarbosa/todolist/config/WebConfig.java
+++ src/main/java/deb/gabrielbarbosa/todolist/config/WebConfig.java
@@ -6,18 +6,16 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig {
-
-    @Value("${cors.origins}")
-    private String corsOrigins;
-
+    
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer corsFilter() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+                registry.addMapping("/**").allowedMethods("*");
             }
         };
     }
+
 
 }
